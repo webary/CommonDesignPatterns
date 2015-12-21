3c5a2e49daab35f1ada4d2acc8c35cb54843013b
webary <813577991@qq.com>
2015-12-21 23:31:50 +0800

    Update XMLUtil.java

--- 操作系统处理图像-桥接模式/src/XMLUtil.java
+++ 操作系统处理图像-桥接模式/src/XMLUtil.java
@@ -3,17 +3,17 @@ import org.w3c.dom.*;
 import java.io.*;
 public class XMLUtil
 {
-//¸Ã·½·¨ÓÃÓÚ´ÓXMLÅäÖÃÎÄ¼şÖĞÌáÈ¡¾ßÌåÀàÀàÃû£¬²¢·µ»ØÒ»¸öÊµÀı¶ÔÏó
+	//è¯¥æ–¹æ³•ç”¨äºä»XMLé…ç½®æ–‡ä»¶ä¸­æå–å…·ä½“ç±»ç±»åï¼Œå¹¶è¿”å›ä¸€ä¸ªå®ä¾‹å¯¹è±¡
 	public static Object getBean(String args)
 	{
 		try
 		{
-			//´´½¨ÎÄµµ¶ÔÏó
+			//åˆ›å»ºæ–‡æ¡£å¯¹è±¡
 			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dFactory.newDocumentBuilder();
 			Document doc = builder.parse(new File("config.xml")); 
 			
-			//»ñÈ¡°üº¬ÀàÃûµÄÎÄ±¾½Úµã
+			//è·å–åŒ…å«ç±»åçš„æ–‡æœ¬èŠ‚ç‚¹
 			NodeList nl = null;
 			if(args.equals("ImageFile"))
 			{
@@ -25,7 +25,7 @@ public class XMLUtil
 			}
             Node classNode=nl.item(0).getFirstChild();
             String cName=classNode.getNodeValue();
-	        //Í¨¹ıÀàÃûÉú³ÉÊµÀı¶ÔÏó²¢½«Æä·µ»Ø
+	        //é€šè¿‡ç±»åç”Ÿæˆå®ä¾‹å¯¹è±¡å¹¶å°†å…¶è¿”å›
 	        Class<?> c = Class.forName(cName);
 		  	Object obj = c.newInstance();
 	        return obj;		
